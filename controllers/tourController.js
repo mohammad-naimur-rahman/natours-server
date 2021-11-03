850b5cd198322381be9b6be4b61d70fba5a8cdd7
Naimur Rahman <naeemhasan28@gmail.com>
2021-11-03 14:56:49 +0600

    added aggregation pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -22,12 +22,12 @@ exports.getAllTours = async (req, res) => {
     res.status(200).json({
       status: 'success',
       results: tours.length,
-      data: { tours },
+      data: { tours }
     })
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err,
+      message: err
     })
   }
 }
@@ -37,12 +37,12 @@ exports.getTour = async (req, res) => {
     const tour = await Tour.findById(req.params.id)
     res.status(200).json({
       status: 'success',
-      data: { tour },
+      data: { tour }
     })
   } catch (err) {
     res.status(404).json({
       status: 'fail',
-      message: err,
+      message: err
     })
   }
 }
@@ -52,12 +52,12 @@ exports.createTour = async (req, res) => {
     const newTour = await Tour.create(req.body)
     res.status(201).json({
       status: 'success',
-      data: { tour: newTour },
+      data: { tour: newTour }
     })
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: err,
+      message: err
     })
   }
 }
@@ -66,16 +66,16 @@ exports.updateTour = async (req, res) => {
   try {
     const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
-      runValidators: true,
+      runValidators: true
     })
     res.status(201).json({
       status: 'success',
-      data: { tour: updatedTour },
+      data: { tour: updatedTour }
     })
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: err,
+      message: err
     })
   }
 }
@@ -84,28 +84,98 @@ exports.deleteTour = async (req, res) => {
   try {
     await Tour.findByIdAndDelete(req.params.id)
     res.status(204).json({
-      message: 'success',
+      message: 'success'
     })
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: err,
+      message: err
     })
   }
 }
 
 exports.getTourStats = async (req, res) => {
   try {
-    const stats = Tour.aggregate([
+    const stats = await Tour.aggregate([
       {
-        $match: { ratingsAverage: { $gte: 4.5 } },
+        $match: {
+          ratingsAverage: { $gte: 4.5 }
+        }
       },
       {
         $group: {
-          _id: null,
+          _id: '$difficulty',
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
           avgRating: { $avg: '$ratingsAverage' },
-        },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' }
+        }
       },
+      {
+        $sort: { avgPrice: 1 }
+      }
     ])
-  } catch (err) {}
+
+    res.status(200).json({
+      status: 'success',
+      data: { stats }
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    })
+  }
+}
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates'
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`)
+          }
+        }
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: '$name' }
+        }
+      },
+      {
+        $addFields: { month: '$_id' }
+      },
+      {
+        $project: {
+          _id: 0
+        }
+      },
+      {
+        $sort: { numTourStarts: -1 }
+      },
+      {
+        $limit: 12
+      }
+    ])
+
+    res.status(200).json({
+      status: 'success',
+      data: { plan }
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    })
+  }
 }
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,11 +7,13 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
+  getMonthlyPlan
 } = tourController
 const router = express.Router()
 
-//router.param('id', checkId)
-
+router.route('/tour-stats/').get(getTourStats)
+router.route('/monthly-plan/:year').get(getMonthlyPlan)
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours)
 router.route('/').get(getAllTours).post(createTour)
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour)
