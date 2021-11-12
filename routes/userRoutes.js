1b7246e6a06d9e4e3b25768066dd5de3b6b78eda
Naimur Rahman <naeemhasan28@gmail.com>
2021-11-12 17:31:04 +0600

    added update password and update user data

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const userController = require('./../controllers/userController')
 const authController = require('./../controllers/authController')
-const { getAllUsers, createUser, getUser, updateUser, deleteUser } =
+const { getAllUsers, createUser, getUser, updateUser, deleteUser, updateMe } =
   userController
-const { signup, login, forgotPassword, resetPassword } = authController
+const {
+  signup,
+  login,
+  forgotPassword,
+  resetPassword,
+  protect,
+  updatePassword
+} = authController
 const router = express.Router()
 
 router.post('/signup', signup)
@@ -11,6 +18,8 @@ router.post('/login', login)
 
 router.post('/forgotPassword', forgotPassword)
 router.patch('/resetPassword/:token', resetPassword)
+router.patch('/updateMyPassword', protect, updatePassword)
+router.patch('/updateMe', protect, updateMe)
 
 router.route('/').get(getAllUsers).post(createUser)
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser)
