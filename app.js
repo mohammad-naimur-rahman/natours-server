d54a0a736d6785becfb2d1ce72c77f11f785f7b8
Naimur Rahman <naeemhasan28@gmail.com>
2021-11-14 18:59:08 +0600

    added parameter polution prevention

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
 const mongoSanitize = require('express-mongo-sanitize')
 const xss = require('xss-clean')
+const hpp = require('hpp')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -18,6 +19,7 @@ app.use(express.json({ limit: '10kb' }))
 app.use(helmet())
 app.use(mongoSanitize())
 app.use(xss())
+app.use(hpp())
 
 if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'))
 
