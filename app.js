de9fee6c8a1e37120c0ef4271d66479ce2282fe0
Naimur Rahman <naeemhasan28@gmail.com>
2021-11-14 18:44:37 +0600

    added helmet

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -11,7 +12,9 @@ const userRouter = require('./routes/userRoutes')
 const app = express()
 
 //-- Global Middlewares
-app.use(express.json())
+app.use(express.json({ limit: '10kb' }))
+
+app.use(helmet())
 
 if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'))
 
