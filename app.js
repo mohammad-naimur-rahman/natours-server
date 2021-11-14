f79cf9145c9e007cf0143163de019f3238e90a4e
Naimur Rahman <naeemhasan28@gmail.com>
2021-11-14 18:51:40 +0600

    added data sanitization and xss

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
+const mongoSanitize = require('express-mongo-sanitize')
+const xss = require('xss-clean')
 
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
@@ -13,8 +15,9 @@ const app = express()
 
 //-- Global Middlewares
 app.use(express.json({ limit: '10kb' }))
-
 app.use(helmet())
+app.use(mongoSanitize())
+app.use(xss())
 
 if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'))
 
