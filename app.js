cb20063ac5b30a0cbbb23aee97dbc82c9bfdcf13
Naimur Rahman <naeemhasan28@gmail.com>
2021-11-14 18:33:12 +0600

    added api limiting

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
+const rateLimit = require('express-rate-limit')
+
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/errorController')
 
@@ -8,10 +10,18 @@ const userRouter = require('./routes/userRoutes')
 
 const app = express()
 
-//-- Middlewares
+//-- Global Middlewares
 app.use(express.json())
 
 if (process.env.NODE_ENV !== 'production') app.use(morgan('dev'))
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+})
+
+app.use('/api', limiter)
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString()
