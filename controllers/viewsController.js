73c377a138522d1a70e161eca662734be0678c8a
Naimur Rahman <naeemhasan28@gmail.com>
2021-11-17 14:52:48 +0600

    added tour page without the map

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -13,6 +13,18 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   })
 })
 
-exports.getTour = (req, res) => {
-  res.status(200).render('tour')
-}
+exports.getTour = catchAsync(async (req, res, next) => {
+  // 1) Get the data, for the requested tour (including reviews and guides)
+  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
+    path: 'reviews',
+    fields: 'review rating user'
+  })
+  // 2) Build template
+
+  // 3) Render template using data from 1)
+
+  res.status(200).render('tour', {
+    title: `${tour.name} Tour`,
+    tour
+  })
+})
