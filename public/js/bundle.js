d818088e317ecb8f2246ce59d0717b4757c2d457
Naimur Rahman <naeemhasan28@gmail.com>
2021-11-20 21:41:27 +0600

    added forgot password and reset password functionality

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -9368,7 +9368,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55505" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53342" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
